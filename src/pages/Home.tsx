7c762b3854c84b3e8eb4856c4946822a4f87dcc0
Vandré Siqueira <vandreifpe@gmail.com>
2021-07-01 15:24:16 -0300

    Dia 2 concluído

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,4 @@
-import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
-
-import { auth, firebase } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -9,24 +6,23 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
-import { TestContext } from '../App';
 
 import '../styles/auth.scss';
+import { useAuth } from '../hooks/useAth';
+
+
 
 export function Home() {
     const history = useHistory();
-    const value = useContext(TestContext);
+    const { user, signInWithGoogle } = useAuth();
 
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GoogleAuthProvider();
+    async function handleCreateRoom() {
+        if(!user) {
+            await signInWithGoogle();
+        }
 
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result);
 
-            history.push('/rooms/new');
-        })
-
-        
+        history.push('/rooms/new');        
     }
 
     return (
@@ -37,7 +33,6 @@ export function Home() {
                <p>Tire as dúvidas da sua audiência em tempo real</p>
             </aside>
             <main>
-                <h1>{value}</h1>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
